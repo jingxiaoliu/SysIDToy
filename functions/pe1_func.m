cf7198f04fa994dc0d411dd1f72752022936e68c
Jingxiao Liu <jingxial@andrew.cmu.edu>
2019-06-21 10:43:00 -0400

    add SIM

--- functions/pe1_func.m
+++ functions/pe1_func.m
@@ -8,12 +8,14 @@
 % Input: 
 %       params - system parameters
 %       tspan  - time span
+%       x0 - 4 X 1 state initialization
 %       F  - 2 X nt system input (force)
+%       C  - 4 X 4 observation matrix
 %       y_true - sampled observation
 % output: 
 %       pe - prediction error
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function pe = pe_func(params,tspan,F,y_true)
+function pe = pe1_func(params,tspan,x0,C,F,y_true)
     m1 = params(1);
     m2 = params(2);
     k1 = params(3);
@@ -24,7 +26,7 @@ function pe = pe_func(params,tspan,F,y_true)
     K=[k1+k2 -k2; -k2 k2]; %stiffness matrix
     Damp=[c1+c2 -c2; -c2 c2]; % damping matrix
     dt = tspan(2)-tspan(1); % delta time
-    [dim_obs,~] = size(y_true);
+    [dim_obs,~] = size(C);
     Ac = [0,0,1,0;
          0,0,0,1;
          -K(1,1)/M(1,1),-K(1,2)/M(1,1),-Damp(1,1)/M(1,1),-Damp(1,2)/M(1,1);
@@ -36,9 +38,12 @@ function pe = pe_func(params,tspan,F,y_true)
         0,0,1/M(1,1),0;
         0,0,0,1/M(2,2)];
     B = inv(Ac)*(A-eye(4))*Bc;    
-    yp = zeros(dim_obs,length(tspan));
+    state = zeros(4,length(tspan));
+    ysol = zeros(dim_obs,length(tspan));
+    state(:,1) = x0;
     for i = 1:1:length(tspan)-1
-        yp(:,i+1) = A*y_true(:,i)+B*[0;0;F(1,i);F(2,i)];
+        state(:,i+1) = A*state(:,i)+B*[0;0;F(1,i);F(2,i)];
+        ysol(:,i+1) = C*state(:,i+1);
     end    
-    pe = norm(yp-y_true,2);
+    pe = norm(ysol-y_true,2);
 end
